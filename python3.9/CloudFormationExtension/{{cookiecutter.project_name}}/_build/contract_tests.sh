9db6ddd29f20800b0b1febfaf887fc5addfe80e3
Nick Carpenter <nick.carpenter@dotmatics.com>
2023-10-16 08:06:00 -0400

    Updates to latest versions

--- python3.9/CloudFormationExtension/{{cookiecutter.project_name}}/_build/contract_tests.sh
+++ python3.9/CloudFormationExtension/{{cookiecutter.project_name}}/_build/contract_tests.sh
@@ -1,20 +1,5 @@
 set -e
 
 echo "Starting CFN Contract Tests"
-#Currently requires environment variables - Will support --profile in releases above 0.2.28 - https://github.com/aws-cloudformation/cloudformation-cli/issues/965
-if [[ -z "${AWS_ACCESS_KEY_ID}" ]]; then
-  echo "Failed to find AWS_ACCESS_KEY_ID env variable"
-  exit 1
-fi
 
-if [[ -z "${AWS_SECRET_ACCESS_KEY}" ]]; then
-  echo "Failed to find AWS_SECRET_ACCESS_KEY env variable"
-  exit 1
-fi
-
-if [[ -z "${AWS_SESSION_TOKEN}" ]]; then
-  echo "Failed to find AWS_SESSION_TOKEN env variable"
-  exit 1
-fi
-
-cfn test
+cfn test --profile IAC1DEV
--- python3.9/CloudFormationExtension/{{cookiecutter.project_name}}/_build/start_sam.sh
+++ python3.9/CloudFormationExtension/{{cookiecutter.project_name}}/_build/start_sam.sh
@@ -1,4 +1,4 @@
 set -e
 
 echo "Starting SAM for Contract Tests"
-sam local start-lambda -l log.log --region eu-west-2
+sam local start-lambda -l log.log --region us-east-1
